22307c0cc43813742c97e35bdb9b702a4496c230
sco. <77859647+scovings@users.noreply.github.com>
2025-03-03 17:16:52 +0000

    update gitignore

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,12 @@ import express from "express";
 const port = process.env.PORT || 3000;
 const app = express();
 
+const imagekit = new ImageKit({
+  urlEndpoint: 'process.env.IMAGE_KIT_ENDPOINT',
+  publicKey: '',
+  privateKey: ''
+});
+
 app.get("/api/upload",(req,res)=>{
   res.send("it works!")
 })
